85a9d7bb36967be94724b5970409b6013c249f43
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-17 13:03:25 -0700

    print base16 numbers in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c, n;
+	
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
