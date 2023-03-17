9b7e1922d04cb391ee7480c5358917ea20bcfc2a
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-17 12:14:00 -0700

    print the alphabet in lower case followed with uppercase (for and while loops)

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char i = 'a';
+
+        while (i <= 'z')
+        {
+                putchar(i);
+                i++;
+        }
+	
+	for (i = 'A';i < 'Z';i++)
+	{
+		putchar(i);
+	}
+
+        putchar('\n');
+
+        return (0);
+}
