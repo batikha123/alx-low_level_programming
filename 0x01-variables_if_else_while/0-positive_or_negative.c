ab37a0959e5f634468980c71400ef3b97848be46
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-17 11:18:11 -0700

    if else conditions

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */ 
+ */
 int main(void)
 {
 	int n;
@@ -16,11 +16,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n<0)
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
