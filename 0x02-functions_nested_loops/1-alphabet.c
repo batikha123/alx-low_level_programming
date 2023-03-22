2280285e233eb7fb535cebf57c72275c28ed0ecb
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-21 17:00:10 -0700

    Printing the alphabet using defined function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Print the alphabet + \n
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
