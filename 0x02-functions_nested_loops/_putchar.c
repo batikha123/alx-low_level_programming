75f86fbee81deaf7b5dc0fc0544587483b2ee858
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-21 17:53:01 -0700

    Updated _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _putchar - Entry point
- *
- * prints c
- *
- * Return: void
+ * _putchar - Print a character 
+ * @c: the character to be printed
  */
 void _putchar(char c)
 {
