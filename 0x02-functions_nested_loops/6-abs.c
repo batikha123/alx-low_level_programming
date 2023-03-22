d424d645b3410612119958568df68bd9cef60315
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-21 18:20:02 -0700

    Print the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - Compute the absolute value of a number
+ * @n: The number to be computed
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+	int i;
+
+	i = abs(n);
+	return (i);
+}
