89b94996cab65d63824d5aa195e4939917b6e238
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-23 22:47:54 -0700

    print a tringle of n lines

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j < size - i + 1)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
