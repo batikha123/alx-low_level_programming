2628b443b5a8e3d250b1691529d26c8fb34376a2
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-27 20:33:25 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copy a string into another string
+ * @dest: the string to copy to
+ * @src: the string to copy
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
