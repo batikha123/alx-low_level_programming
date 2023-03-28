8c999bad725d544bbac3fe1dfb28b6dc69a04fb3
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-28 08:08:35 -0700

    atoi implementation

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _atoi - convert a string into an integer
+ * @s: the string to convert
+ * Return: an integer
+ */
+int _atoi(char *s)
+{
+	int i, d, num, c;
+
+	num = 0;
+	c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (isdigit(s[i]))
+		{
+			d = s[i] - '0';
+			num = num * 10 + d;
+
+			if (isdigit(s[i + 1]) == 0)
+				break;
+		}
+		else if (s[i] == '-')
+			c++;
+		else
+			continue;
+	}
+
+	if (c % 2 != 0)
+		num *= -1;
+
+	return (num);
+}
