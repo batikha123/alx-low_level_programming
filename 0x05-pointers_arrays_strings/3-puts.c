033aec7167ce5629b68fde14f7cd0ece65ec7a2b
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-27 18:51:26 -0700

    print a string passed to a function

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,15 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 /**
  * _puts - print a string + \n
  * @str: the string
  */
 void _puts(char *str)
 {
-	_putchar(str);
+	while(*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-void _putchar(const char *str);
+void _putchar(char str);
 void print_rev(char *s);
 
 #endif
