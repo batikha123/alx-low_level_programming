1016b093fd05480302235a3e6e30df2b0767cab0
Youssef Abbar <abbaryoussef345@gmail.com>
2023-03-30 18:50:41 -0700

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, c;
+
+	c = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+			continue;
+		else
+		{
+			c = s1[i] - s2[i];
+			break;
+		}
+	}
+
+	return (c);
+}
