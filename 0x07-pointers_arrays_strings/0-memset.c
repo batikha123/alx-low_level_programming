13739e604aa82b941290e04e42ba0faaacee859d
Youssef Abbar <abbaryoussef345@gmail.com>
2023-04-03 15:25:13 -0700

    implementing memset()

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - fill memory with a constent byte
+ * @s: starting address of memory to be filled
+ * @b: Value to be filled
+ * @n: number of bytes to be filled in the memory
+ * Return:
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
