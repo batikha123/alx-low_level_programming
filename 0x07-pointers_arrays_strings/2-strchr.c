bacf97607f2aa67a54d9886331ee04cfc6c2dc8e
Youssef Abbar <abbaryoussef345@gmail.com>
2023-04-03 16:34:33 -0700

    implementing strchr()

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
  * _strchr - locate a character in a string
  * @s: the string
  * @c: the character
- * Return: pointer to adresse of the first occurence of c or NULL if not exists
+ * Return: pointer to the first occurence of c or NULL if not exists
  */
 char *_strchr(char *s, char c)
 {
@@ -14,8 +14,8 @@ char *_strchr(char *s, char c)
 		s++;
 	}
 
-	if (*s != '\0')
-		return (s);
-	else
+	if (*s == '\0')
 		return (NULL);
+	else
+		return (s);
 }
