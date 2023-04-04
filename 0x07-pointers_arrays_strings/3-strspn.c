9bc49e2dcd3f97b93fea086b6f1074c6ae524d63
Youssef Abbar <abbaryoussef345@gmail.com>
2023-04-03 17:49:05 -0700

    implementing strspn()

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,17 +8,15 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, c, k;
-	char *p;
 
-	p = accept;
 	c = 0;
 
-	for (i = 0; p[i] != '\0'; i++)
+	for (i = 0; accept[i] != '\0'; i++)
 		for (j = 0; s[j] != '\0'; j++)
-			if (p[i] == s[j])
+			if (accept[i] == s[j])
 			{
 				k = j;
-				while (s[k + 1] == p[i])
+				while (s[k + 1] == accept[i])
 				{
 					c++;
 					k++;
