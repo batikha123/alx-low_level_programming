0451d4b3ccef28349b505fbe9402cc7d04278bb4
Youssef Abbar <abbaryoussef345@gmail.com>
2023-04-17 18:37:36 -0700

    Return a pointer to a new struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,9 +16,45 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
+	dog->name = malloc(sizeof(char) * _strlen(dog->name));
+
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * _strlen(dog->owner));
+
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
 
 	return (dog);
+}
+/**
+ * _strlen - determine the length of a string
+ * @str: the string
+ * Return: integer
+ */
+int _strlen(char *str)
+{
+	int c = 0;
+
+	if (str == NULL)
+		return (c);
+	while (*str)
+	{
+		c++;
+		str++;
+	}
+
+	return (c);
 }
