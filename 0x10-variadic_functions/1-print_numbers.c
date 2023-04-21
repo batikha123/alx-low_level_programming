5ab9ff22b6c3261895d18b8c64e3733e8af5ee81
Youssef Abbar <abbaryoussef345@gmail.com>
2023-04-20 18:47:53 -0700

    Vardiac functions that prints its arguments separated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,9 +18,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		tmp = va_arg(args, int);
 		printf("%d", tmp);
 
-		if (i == n - 1)
+		if (separator == NULL || i == n - 1)
 			continue;
-
 		printf("%s", separator);
 	}
 	printf("\n");
