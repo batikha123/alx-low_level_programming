27f888980b0ad43d4b1b73a9eb87acc3f2dffebe
Youssef Abbar <abbaryoussef345@gmail.com>
2023-04-21 18:39:38 -0700

    updated header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef _FUNCTION_POINTERS_
 #define _FUNCTION_POINTERS_
 
-char _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
