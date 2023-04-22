7079a42a78eafc8a3d73d73323fd3a183175f5f6
Youssef Abbar <abbaryoussef345@gmail.com>
2023-04-21 18:27:15 -0700

    printing a name using function pointers

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+
+#endif /* #ifndef _FUNCTION_POINTERS_ */
