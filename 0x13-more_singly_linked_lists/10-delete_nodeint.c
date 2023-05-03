67f3edcda67392fe213c2e8c7c5e70e45811be99
Youssef Abbar <abbaryoussef345@gmail.com>
2023-05-03 09:00:10 -0700

    fixing betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,40 +8,39 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current, *prev;
-    unsigned int i = 0;
+	listint_t *current, *prev;
+	unsigned int i = 0;
 
-    if (*head == NULL || head == NULL)
-        return (-1);
+	if (*head == NULL || head == NULL)
+		return (-1);
 
-    current = *head;
+	current = *head;
 
-    if (index == 0)
-    {
-        (*head) = (*head)->next;
-        free(current);
-        return (1);
-    }
+	if (index == 0)
+	{
+		(*head) = (*head)->next;
+		free(current);
+		return (1);
+	}
 
-    prev = NULL;
+	prev = NULL;
 
-    while (current)
-    {
-        if (i == index)
-        {
-            if (prev == NULL)
-                (*head) = current->next;
-            else
-                prev->next = current->next;
+	while (current)
+	{
+		if (i == index)
+		{
+			if (prev == NULL)
+				(*head) = current->next;
+			else
+				prev->next = current->next;
 
-            free(current);
-            return(1);
-        }
+			free(current);
+			return (1);
+		}
+		i++;
+		prev = current;
+		current = current->next;
+	}
 
-        i++;
-        prev = current;
-        current = current->next;
-    }
-
-    return (-1);
+	return (-1);
 }
