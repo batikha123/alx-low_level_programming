68c4b705ba2340bb2347d582424e13b18f09213e
Youssef Abbar <abbaryoussef345@gmail.com>
2023-05-03 04:11:25 -0700

    inserting a node at a giving index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,14 +20,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-	
+
 	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 		return (new);
 	}
-	
+
 	current = *head;
 
 	while (current)
