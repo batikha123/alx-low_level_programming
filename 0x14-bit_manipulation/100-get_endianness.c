6a64605e2a642730e9676862666eb1c9d3296902
Youssef Abbar <abbaryoussef345@gmail.com>
2023-05-05 18:46:59 -0700

    Check for endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *ptr = (char *)&num;
+
+	return (*ptr == 1);
+}
